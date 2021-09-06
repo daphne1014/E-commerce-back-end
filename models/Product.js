5b173b5ff5b5a4e39bdd30055ee92b2e00f03616
Daphne Song <85268906+daphne1014@users.noreply.github.com>
2021-09-06 00:50:53 -0700

    modified product model

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,41 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 160]
+      }
+    },
+    price: {
+      type: DataTypes.NUMBER,
+      allowNull: false,
+      validate: {
+        isNumeric: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isInt: true,
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references:{
+        model:'category',
+        key: 'id'
+      }
+    },
+    tags: {
+      type: DataTypes.INTEGER,
+      references:{
+        model:'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
